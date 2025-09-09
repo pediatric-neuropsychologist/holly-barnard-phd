1d30ee34f2453b3dc17797c205ae625c37a1ae6e
coderrob <rob@coderrob.com>
2025-09-09 01:53:50 -0500

    chore: basic migration to astrojs

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://<USERNAME>.github.io/<REPO>', // update this to your GitHub Pages URL
+  publicDir: 'public'
+});
